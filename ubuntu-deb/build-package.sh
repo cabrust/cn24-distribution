9029edd09fd4656b05887a55dfe4dc3abe58c123
Clemens-Alexander Brust <clemens-alexander.brust@uni-jena.de>
2017-04-06 16:03:54 +0200

    Debian/Ubuntu distribution is now working

--- ubuntu-deb/build-package.sh
+++ ubuntu-deb/build-package.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
-  echo "USAGE: $0 <cn24 source folder> <cn24 version> <package version> <ubuntu version>"
+  echo "USAGE: $0 <cn24 source folder> <cn24 version suffix> <package version> <ubuntu version>"
   exit
 fi
 
 CN24_ROOT=$1
-CN24_VERSION=$2
+CN24_VERSION_SUFFIX=$2
+if [ -e $CN24_ROOT/VERSION ]; then
+  CN24_VERSION=$(cat $CN24_ROOT/VERSION)$CN24_VERSION_SUFFIX
+else
+  CN24_VERSION=$CN24_VERSION_SUFFIX
+fi
 PACKAGE_VERSION=$3
 UBUNTU_VERSION=$4
+if [ -z "$CN24_CONFIGS" ]; then
+  CN24_CONFIGS=gpu cpu gpu-opencv
+fi
+
 echo "Choosing CN24 install from $CN24_ROOT (Version $CN24_VERSION)"
 echo "Using Ubuntu $UBUNTU_VERSION"
 
@@ -23,6 +32,7 @@ TMP_BU=$(pwd)
 cd $CN24_ROOT
 CN24_SRC=$(pwd)
 cd "$TMP_BU"
+mkdir -p dist-tmp
 
-docker run -it --name cn24pkgbuilder -v $CN24_SRC:/cn24-pkg-out/cn24-src -v $(pwd)/debian:/cn24-pkg-out/debian -e "CN24_VERSION=$CN24_VERSION" -e "PACKAGE_VERSION=$PACKAGE_VERSION" cn24pkg:$UBUNTU_VERSION bash /cn24-pkg-out/build-package.sh
+docker run -it --name cn24pkgbuilder -v $CN24_SRC:/cn24-pkg-out/cn24-src -v $(pwd)/debian:/cn24-pkg-out/debian -v $(pwd)/dist-tmp:/cn24-pkg-out/dist-tmp -v ~/.gnupg:/root/.gnupg -e "CN24_VERSION=$CN24_VERSION" -e "PACKAGE_VERSION=$PACKAGE_VERSION" -e "CN24_CONFIGS=$CN24_CONFIGS" cn24pkg:$UBUNTU_VERSION bash /cn24-pkg-out/build-package.sh
 docker rm cn24pkgbuilder
--- ubuntu-deb/scripts/build-package.sh
+++ ubuntu-deb/scripts/build-package.sh
@@ -2,31 +2,91 @@
 
 # This script runs inside the container
 cd "${0%/*}"
-
-echo "Making target directory"
-mkdir cn24-$CN24_VERSION
-cd cn24-$CN24_VERSION
-
-# Copy files from repo
-echo "Copying files..."
 shopt -s extglob
-cp -r ../cn24-src/!(distribution) ./
 
-echo "Moving debian control files..."
-cp -r ../debian debian
+for CN24_CONFIG in $CN24_CONFIGS; do
+  echo "Making target directory"
+  mkdir cn24-$CN24_CONFIG-$CN24_VERSION
+  cd cn24-$CN24_CONFIG-$CN24_VERSION
 
-DISTRO_CODE=$(lsb_release -c -s)
-echo "Adding changelog info... ($DISTRO_CODE)"
-echo "cn24 ($CN24_VERSION-$PACKAGE_VERSION~$DISTRO_CODE) $DISTRO_CODE; urgency=low" > debian/changelog
-echo "" >> debian/changelog
-echo "  * Change" >> debian/changelog
-echo "" >> debian/changelog
-echo " -- Clemens-Alexander Brust <clemens-alexander.brust@uni-jena.de> $(date -R)" >> debian/changelog
+  # Copy files from repo
+  echo "Copying files..."
+  cp -r ../cn24-src/!(distribution) ./
 
-# Create "origin" tarball
-echo "Creating origin tarball..."
-tar -cz -f ../cn24_$CN24_VERSION.orig.tar.gz *
+  # Make origin tarball appear
+  echo "Building package..."
+  if [ $PACKAGE_VERSION -eq 1 ]; then
+    echo "Creating origin tarball..."
+    tar -cz -f ../cn24-${CN24_CONFIG}_$CN24_VERSION.orig.tar.gz *
+  else
+    echo "Downloading origin tarball..."
+    wget https://launchpad.net/~cn24-team/+archive/ubuntu/ppa/+files/cn24-${CN24_CONFIG}_$CN24_VERSION.orig.tar.gz -P ..
+    if [ ! -e ../cn24-${CN24_CONFIG}_$CN24_VERSION.orig.tar.gz ]; then
+      echo "Could not find original tarball on launchpad, rebuilding..."
+      touch ../cn24-${CN24_CONFIG}_$CN24_VERSION-orig-missing
+      tar -cz -f ../cn24-${CN24_CONFIG}_$CN24_VERSION.orig.tar.gz *
+    fi
+  fi
 
-echo "Building package..."
-debuild -us -uc -j12
+  echo "Moving debian control files..."
+  cp -r ../debian debian
+
+  DISTRO_CODE=$(lsb_release -c -s)
+  echo "Adding changelog info... ($DISTRO_CODE)"
+  echo "cn24-$CN24_CONFIG ($CN24_VERSION-$PACKAGE_VERSION~$DISTRO_CODE) $DISTRO_CODE; urgency=low" > debian/changelog
+  echo "" >> debian/changelog
+  echo "  * See CN24 wiki for detailed changelog" >> debian/changelog
+  echo "" >> debian/changelog
+  echo " -- Clemens-Alexander Brust <clemens-alexander.brust@uni-jena.de>  $(date -R)" >> debian/changelog
+  
+  echo "Moving appropriate files"
+  cp debian/rules.$CN24_CONFIG debian/rules
+  cp debian/control.$CN24_CONFIG debian/control
+  cp debian/cn24.install debian/cn24-${CN24_CONFIG}.install
+  cp debian/cn24-dev.install debian/cn24-${CN24_CONFIG}-dev.install
+  cp debian/cn24.install debian/cn24-${CN24_CONFIG}.dirs
+  cp debian/cn24-dev.install debian/cn24-${CN24_CONFIG}-dev.dirs
+
+  # Create "origin" tarball
+
+  echo "Building package..."
+  if [ $PACKAGE_VERSION -eq 1 ]; then
+    debuild -us -uc -j12
+  else
+    if [ -e ../cn24-${CN24_CONFIG}_$CN24_VERSION-orig-missing ]; then
+      debuild -us -uc -j12
+    else
+      debuild -sd -us -uc -j12
+    fi
+  fi
+
+  cd ..
+  ls *.deb
+done
+
+echo "Signing packages..."
+for CN24_CONFIG in $CN24_CONFIGS; do
+  cd cn24-$CN24_CONFIG-$CN24_VERSION
+  if [ $PACKAGE_VERSION -eq 1 ]; then
+    debuild -S
+  else
+    if [ -e ../cn24-${CN24_CONFIG}_$CN24_VERSION-orig-missing ]; then
+      debuild -S
+    else
+      debuild -sd -S
+    fi
+  fi
+  cd ..
+done
+
+cp *.deb dist-tmp/
+cp *.changes dist-tmp/
+cp *.tar.gz dist-tmp/
+echo "To upload your package to the ppa, run the following commands:"
+for CN24_CONFIG in $CN24_CONFIGS; do
+  echo "dput ppa:cn24-team/ppa cn24-${CN24_CONFIG}_$CN24_VERSION_*_source.changes"
+done
+
+echo 'Otherwise, you can just type "exit" at the command prompt.'
+
 bash
